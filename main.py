1ff54cadf85a28c5e6b60fc9a9f4bcd3cc60fc1d
toledompm <toledompm@gmail.com>
2021-02-11 21:08:27 -0300

    black format

--- main.py
+++ main.py
@@ -2,29 +2,32 @@ import requests
 import sys
 import json
 
+
 def fetch_data(url, retries=0):
-    if retries > 2: 
-        raise('Err, too many retries')
+    if retries > 2:
+        raise ("Err, too many retries")
 
     try:
         r = requests.get(url)
         r.raise_for_status()
     except requests.exceptions.Timeout:
-        print('timeout, retrying...')
+        print("timeout, retrying...")
         fetch_data(url, retries + 1)
     except requests.exceptions.HTTPError as e:
         raise SystemExit(e)
 
-    file = open('resp.json', 'w')
+    file = open("resp.json", "w")
     file.write(r.text)
     file.close()
 
 
 def main():
-    if '-f' or '--fetch' in sys.argv: fetch_data('a real url :D')
-    with open('resp.json') as f:
+    if "-f" or "--fetch" in sys.argv:
+        fetch_data("a real url :D")
+    with open("resp.json") as f:
         data_dict = json.load(f)
 
     print(len(data_dict))
+
 
 main()
